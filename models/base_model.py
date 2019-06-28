6f1b0890e0a661f1fefaac58cce023b51b860298
Wolf Hopkins <zevrov.719@gmail.com>
2019-06-28 09:30:10 -0400

    nvm

--- models/base_model.py
+++ models/base_model.py
@@ -9,25 +9,12 @@ from datetime import datetime
 
 class BaseModel:
 	"""BaseModel for other classes"""
-	def __init__(self, *args, **kwargs):
+	def __init__(self):
 		"""public instance attr"""
 		time = '%Y-%m-%dT%H:%M:%S.%f'
-		if kwargs:
-			for key, value in kwargs.items():
-				if key != '__class__':
-					setattr(self, key, value)
-		if 'id' in kwargs.keys():
-			self.id = kwargs['id']
-			if 'created_at' in kwargs.keys():
-				self.created_at = datetime.strptime(kwargs['created_at'],
-													time)
-			if 'updated_at' in kwargs.keys():
-				self.updated_at = datetime.strptime(kwargs['updated_at'],
-													time)
-		else:
-			self.id = str(uuid.uuid4())
-			self.created_at = datetime.now()
-			self.updated_at = self.created_at
+		self.id = str(uuid.uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = self.created_at
 
 	def __str__(self):
 		"""String rep of the BaseModel"""
