c46bbaad7ceb340c1253c3067e108ac8a17807fe
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-01 13:22:24 -0400

    got file storage to work, syntax error with spaces and tabs fixed, now works with test file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,31 +8,31 @@ import json
 class FileStorage:
 	"""class to store and return obejects using JSON"""
 
-        __file_path = "storage.json"
-        __objects = {}
+	__file_path = "storage.json"
+	__objects = {}
 
-        def all(self):
-            """return the __objects dictionary"""
+	def all(self):
+		"""return the __objects dictionary"""
 
-            return type(self).__objects
+		return type(self).__objects
 
-        def new(self, obj):
-            """creates dictionary of object to __objects"""
+	def new(self, obj):
+		"""creates dictionary of object to __objects"""
 
-            value = obj.to_dict()
-            key = '{}.{}'.format(obj.__class__.name__, obj.id)
-            type(self).__objects[key] = value
+		value = obj.to_dict()
+		key = '{}.{}'.format(obj.__class__.name__, obj.id)
+		type(self).__objects[key] = value
 
-        def reload(self):
-            """retreive objects from a JSON file"""
+	def reload(self):
+		"""retreive objects from a JSON file"""
 
-            if not os.path.isfile(FileStorage.__file_path):
-                return
-            with open(FileStorage.__file_path, 'rt') as file:
-                FileStorage.__objects = json.load(file)
+		if not os.path.isfile(FileStorage.__file_path):
+			return
+		with open(FileStorage.__file_path, 'rt') as file:
+			FileStorage.__objects = json.load(file)
 
-        def save(self):
-            """save to JSON storage file"""
+	def save(self):
+		"""save to JSON storage file"""
 
-            with open(FileStorage.__file_path, 'wt') as file:
-                json.dump(FileStorage.__objects, file)
+		with open(FileStorage.__file_path, 'wt') as file:
+			json.dump(FileStorage.__objects, file)
