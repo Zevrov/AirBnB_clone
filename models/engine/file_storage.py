35e1b85bf44113dc044770b47cb3597f478d4264
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-03 19:00:56 -0400

    got it working again, not sure why it stopped

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,9 +27,8 @@ class FileStorage:
     def new(self, obj):
         """creates dictionary of object"""
         if obj is not None:
-            k = '{}.{}'.format(obj.__class__.name, obj.id)
-            value = obj.to_dict()
-            type(self).__objects[key] = value
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def reload(self):
         """deserializes the JSON file to __objects
@@ -59,8 +58,8 @@ class FileStorage:
     def save(self):
         """save to JSON storage file"""
         new_dict = {}
-        for key in FileStorage.__objects.keys():
-            new_dict[key] = FileStorage.__objects[key].to_dict()
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
         with open(FileStorage.__file_path, mode="w",
                   encoding="UTF-8") as to_file:
             json.dump(new_dict, to_file)
