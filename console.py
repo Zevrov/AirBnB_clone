297238856070afe184c8309af3f6071269839540
Wolf Hopkins <zevrov.719@gmail.com>
2019-07-03 14:14:16 -0400

    pep8 spaces conversion done

--- console.py
+++ console.py
@@ -51,79 +51,79 @@ class HBNBCommand(cmd.Cmd):
             if len(arguments) > 1:
                 keys = arguments[0] + "." + arguments[1]
                 if keys in models.storage.all():
-					print(models.storage.all()[keys])
-				else:
-					print("** no instance found **")
-			else:
-				print("** instance id missing **")
-		else:
-			print("** class doesn't exist **")
+                    print(models.storage.all()[keys])
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
-	def do_destroy(self, arg):
-		"""deletes an instance based on class/id"""
-		arguments = arg.split()
-		if len(arguments) == 0:
-			print("** class name missing **")
-		elif arguments[0] in classes:
-			if len(arguments) > 1:
-				keys = arguments[0] + "." + arguments[1]
-				if keys in models.storage.all()
-				models.storage.all().pop(keys)
-				models.storage.save()
-				else:
-					print("** no instance found **")
-			else:
-				print("** instance id missing **")
-		else:
-			print("** class doesn't exist **")
+    def do_destroy(self, arg):
+        """deletes an instance based on class/id"""
+        arguments = arg.split()
+        if len(arguments) == 0:
+            print("** class name missing **")
+        elif arguments[0] in classes:
+            if len(arguments) > 1:
+                keys = arguments[0] + "." + arguments[1]
+                if keys in models.storage.all()
+                models.storage.all().pop(keys)
+                models.storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
-	def do_all(self, arg):
-		"""prints all instances if they exist"""
-		instances = storage.all()
-		class_arg = "empty"
-		if arg in self.classes:
-			for keys, value in instances.items():
-				if arg in key:
-					print(value)
-					class_arg = "instances exist"
-				if class_arg == "empty":
-					print("[]")
-			else:
-				print("** class doesn't exist **")
-		else:
-			for value in instances.values():
-				print(value)
+    def do_all(self, arg):
+        """prints all instances if they exist"""
+        instances = storage.all()
+        class_arg = "empty"
+        if arg in self.classes:
+            for keys, value in instances.items():
+                if arg in key:
+                    print(value)
+                    class_arg = "instances exist"
+                if class_arg == "empty":
+                    print("[]")
+            else:
+                print("** class doesn't exist **")
+        else:
+            for value in instances.values():
+                print(value)
 
-	def do_update(self, arg):
-		"""updates instance based on class/id"""
-		arguments = arg.split()
-		float = ["latitude", "longitude"]
-		int = ["number_rooms", "number_bathrooms", "max_guest", "price_by_night"]
-		if len(arguments) == 0:
-			print("** class name missing **")
-		elif arguements[0] in classes:
-			if len(arguments) > 1:
-				keys = arguments[0] + "." + arguments[1]
-				if keys in models.storage.all():
-					if len(arguments) > 2:
-						if len(arguments) > 3:
-							if arguments[0] == "Place":
-								if arguments[2] in int:
-									try:
-										arguments[3] = int(arguments[3])
-									except:
-										arguments[3] = 0
-								elif arguments[2] in float:
-									try:
-										arguments[3] = float(arguments[3])
-									except:
-										arguments[3] = 0.0
-							setattr(models.storage.all()[keys], arguments[2], arguemnts[3])
-							models.storage.all()[keys].save()
-						else:
-							print("** value missing **")
-					else:
-						print("** attribute name missing **")
+    def do_update(self, arg):
+        """updates instance based on class/id"""
+        arguments = arg.split()
+        float = ["latitude", "longitude"]
+        int = ["number_rooms", "number_bathrooms", "max_guest", "price_by_night"]
+        if len(arguments) == 0:
+            print("** class name missing **")
+        elif arguements[0] in classes:
+            if len(arguments) > 1:
+                keys = arguments[0] + "." + arguments[1]
+                if keys in models.storage.all():
+                    if len(arguments) > 2:
+                        if len(arguments) > 3:
+                            if arguments[0] == "Place":
+                                if arguments[2] in int:
+                                    try:
+                                        arguments[3] = int(arguments[3])
+                                    except:
+                                        arguments[3] = 0
+                                elif arguments[2] in float:
+                                    try:
+                                        arguments[3] = float(arguments[3])
+                                    except:
+                                        arguments[3] = 0.0
+                            setattr(models.storage.all()[keys], arguments[2], arguemnts[3])
+                            models.storage.all()[keys].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
                 else:
                     print("** no instance found **")
             else:
